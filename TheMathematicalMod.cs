8330177d6874e04ee2769c7bf7114fdae9298dd4
Starblaster11 <Starblaster11@users.noreply.github.com>
2015-10-22 10:48:00 -0600

    Create TheMathematicalMod.cs

---
+++ TheMathematicalMod.cs
@@ -0,0 +1,15 @@
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TheMathematicalMod
+{
+	public class TheMathematicalMod : Mod
+	{
+		public override void SetDefaults()
+		{
+			name = "TheMathematicalMod";
+			properties.autoload = true;
+		}
+	}
+}
