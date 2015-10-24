d5692d14b4b87ac6edb771e93995c54d8af9557a
Starblaster11 <adamgdarling@gmail.com>
2015-10-23 18:31:16 -0600

    Ok.

---
+++ Projectiles/ParabolaProjectile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheMathematicalMod.Projectiles
+{
+    public class ParabolaProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Parabola Projectile";
+        }
+    }
+}
